1f20ea3f3b7a26b6dc9a8b3122f818e0c746d165
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2023-06-19 17:29:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Hintpirox/Shubham.git /Shubham
+git clone https://github.com/NikhilNGY/Shubham.git /Shubham
 cd /Shubham
 pip3 install -r requirements.txt
 echo "Starting Bot...."
